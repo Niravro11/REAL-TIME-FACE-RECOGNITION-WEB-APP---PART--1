2c396d88d1baadbe6086940c3d8b614042bf06c4
Niravro11 <75479734+Niravro11@users.noreply.github.com>
2021-05-17 16:53:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setup() {
     canvas.center();   
     video = createCapture(VIDEO);
     video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7bKjlCZP5/model.json',modelLoaded)
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hr0FQsELV/model.json',modelLoaded)
 }
 function modelLoaded(){
     console.log("modelLoaded!");
